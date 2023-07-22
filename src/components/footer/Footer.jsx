f30da085050a597960fec5959e3956709e32f50b
wizardy <wizardhassan99@gmail.com>
2023-07-22 13:27:58 +0100

    Layouts and Components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div >
+      <div>
+        ©2023 Wizardy Blog. All rights reserved.
+      </div>
+      <div>
+        <span>social media icons</span>
+      </div>
+  </div>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+
+const links = [
+  { id: 1, title: "Home", url: "/" },
+  { id: 2, title: "Portfolio", url: "/portfolio" },
+  { id: 3, title: "Blog", url: "/blog" },
+  { id: 4, title: "About", url: "/about" },
+  { id: 5, title: "Contact", url: "/contact" },
+  { id: 6, title: "Dashboard", url: "/dashboard" },
+];
+
+function Navbar() {
+  return (
+    <div>
+      <Link href="/">
+        My Blog
+      </Link>
+      <div>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
