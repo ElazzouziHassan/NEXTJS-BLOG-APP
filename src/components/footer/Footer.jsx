f1f81ad25e4cb1ad4278fe209aacc910f98ed814
wizardy <wizardhassan99@gmail.com>
2023-07-23 15:23:04 +0100

    updates on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import styles from "./footer.module.css";
 
 function Footer() {
   return (
-    <div >
+    <div className={styles.container}>
       <div>
         ©2023 Wizardy Blog. All rights reserved.
       </div>
-      <div>
+      <div className={styles.social}>
         <span>social media icons</span>
       </div>
   </div>
