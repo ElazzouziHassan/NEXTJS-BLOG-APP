c08c1ad1343efc88fb7a9f2e18e859b34175feca
wizardy <wizardhassan99@gmail.com>
2023-07-23 15:41:02 +0100

    social media links has been added to the project

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from "./footer.module.css";
+import Image from 'next/image';
+import Link from 'next/link';
 
 function Footer() {
   return (
@@ -8,7 +10,39 @@ function Footer() {
         ©2023 Wizardy Blog. All rights reserved.
       </div>
       <div className={styles.social}>
-        <span>social media icons</span>
+        <a
+          href="https://web.facebook.com/itsmewizardy/"
+          target="_blank"
+        >
+          <Image
+            src="/facebook.png"
+            width={15} height={15}
+            className={styles.icon}
+            alt="Author facebook account"
+          />
+        </a>
+        <a
+          href="https://twitter.com/itsmewizardy"
+          target="_blank"
+        >
+          <Image
+            src="/twitter.png"
+            width={15} height={15}
+            className={styles.icon}
+            alt="Author twitter account"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/_wizardy__/"
+          target="_blank"
+        >
+          <Image
+            src="/instagram.png"
+            width={15} height={15}
+            className={styles.icon}
+            alt="Author instagram account"
+          />
+        </a>
       </div>
   </div>
   )
