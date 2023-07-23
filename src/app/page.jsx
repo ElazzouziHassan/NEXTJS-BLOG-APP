225ec951a5a700438acf326b1cecc1e7f89736ba
wizardy <wizardhassan99@gmail.com>
2023-07-23 16:14:50 +0100

    see my works action added to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from "public/hero.png";
+import Button from '@/components/button/button';
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
           Turning your Idea into Reality. We bring together the teams from the
           global tech industry.
         </p>
-        <button url="/portfolio" text="See Our Works"/>
+        <Button url="/portfolio" text="See My Works"/>
       </div>
       <div className={styles.item}>
         <Image src={Hero} alt="" className={styles.img} />
