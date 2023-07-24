db390f93dde48dd63e45bca7be7532de195c9f10
wizardy <wizardhassan99@gmail.com>
2023-07-24 18:14:52 +0100

    updates on portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import styles from "./page.module.css";
+import Link from 'next/link';
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className={styles.container}>
+    <h1 className={styles.selectTitle}>Choose a gallery</h1>
+    <div className={styles.items}>
+      <Link href="/portfolio/illustrations" className={styles.item}>
+        <span className={styles.title}>UI/ UX</span>
+      </Link>
+      <Link href="/portfolio/websites" className={styles.item}>
+        <span className={styles.title}>Databases</span>
+      </Link>
+      <Link href="/portfolio/application" className={styles.item}>
+        <span className={styles.title}>Front-End</span>
+      </Link>
+      <Link href="/portfolio/application" className={styles.item}>
+        <span className={styles.title}>Back-End</span>
+      </Link>
+        
+    </div>
+  </div>
   )
 }
 
