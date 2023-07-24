df1334238cfe472209ef2f9745a17c52ef6be31d
wizardy <wizardhassan99@gmail.com>
2023-07-24 18:39:33 +0100

    dummy data for categories

---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,94 @@
+export const items = {
+  ui_ux: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-1.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-2.jpg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-3.jpg",
+    }
+  ],
+  frontend: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-4.jpg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-1.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-2.jpeg",
+    }
+  ],
+  backend: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-3.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-4.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-1.jpeg",
+    }
+  ],
+  databases: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-3.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-4.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-1.jpeg",
+    }
+  ]
+};
