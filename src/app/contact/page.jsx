81f2f2f2aa7345495a232a86739854f567807d3b
wizardy <wizardhassan99@gmail.com>
2023-07-24 17:38:38 +0100

    contact us form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import styles from "./page.module.css";
+import Button from '@/components/button/Button';
+import Image from 'next/image';
+
+export const metadata = {
+  title: "Wizardy Blog Contact Information",
+  description: "This is Contact Page",
+};
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={styles.container}>
+    <h1 className={styles.title}>Contact us</h1>
+    <div className={styles.content}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt="contact image"
+          fill={true}
+          className={styles.image}
+        />
+      </div>
+      <form className={styles.form}>
+        <input type="text" placeholder="name" className={styles.input} />
+        <input type="text" placeholder="email" className={styles.input} />
+        <textarea
+          className={styles.textArea}
+          placeholder="message"
+          cols="20"
+          rows="6"
+        ></textarea>
+        <Button url="#" text="Send"/>
+      </form>
+    </div>
+  </div>
   )
 }
 
