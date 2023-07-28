86cdb61a3c426e076717e906cbbed97881de6726
wizardy <wizardhassan99@gmail.com>
2023-07-28 13:42:57 +0100

    dummy data updated

--- src/app/portfolio/[category]/data.js
+++ src/app/portfolio/[category]/data.js
@@ -1,94 +1,71 @@
 export const items = {
-  ui_ux: [
+  applications: [
     {
       id: 1,
       title: "Creative Portfolio",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-1.jpeg",
+        "public/data/data-3.jpeg",
     },
     {
       id: 2,
       title: "Minimal Single Product",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image:
-        "public/data/data-2.jpg",
-    },
-    {
-      id: 3,
-      title: "Strong Together Charity",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image:
-        "public/data/data-3.jpg",
-    }
-  ],
-  frontend: [
-    {
-      id: 1,
-      title: "Creative Portfolio",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image:
-        "public/data/data-4.jpg",
-    },
-    {
-      id: 2,
-      title: "Minimal Single Product",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image:
-        "public/data/data-1.jpeg",
-    },
-    {
-      id: 3,
-      title: "Strong Together Charity",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
         "public/data/data-2.jpeg",
-    }
-  ],
-  backend: [
-    {
-      id: 1,
-      title: "Creative Portfolio",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image:
-        "public/data/data-3.jpeg",
-    },
-    {
-      id: 2,
-      title: "Minimal Single Product",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image:
-        "public/data/data-4.jpeg",
     },
     {
       id: 3,
       title: "Strong Together Charity",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-1.jpeg",
+        "public/data/data-4.jpeg",
     }
   ],
-  databases: [
+  illustrations: [
     {
       id: 1,
       title: "Creative Portfolio",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-3.jpeg",
+        "public/data/data-1.jpeg",
     },
     {
       id: 2,
       title: "Minimal Single Product",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-4.jpeg",
+        "public/data/data-2.jpeg",
     },
     {
       id: 3,
       title: "Strong Together Charity",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-1.jpeg",
+        "public/data/data-3.jpeg",
     }
-  ]
+  ],
+  websites: [
+    {
+      id: 1,
+      title: "Creative Portfolio",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-1.jpeg",
+    },
+    {
+      id: 2,
+      title: "Minimal Single Product",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-2.jpeg",
+    },
+    {
+      id: 3,
+      title: "Strong Together Charity",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+      image:
+        "public/data/data-3.jpeg",
+    }
+  ],
 };
