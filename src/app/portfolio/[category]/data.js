30bb785c1f11f706af5bdb062d48f874599dde3a
wizardy <wizardhassan99@gmail.com>
2023-07-28 14:20:11 +0100

    dummy data new updatesss

--- src/app/portfolio/[category]/data.js
+++ src/app/portfolio/[category]/data.js
@@ -5,21 +5,21 @@ export const items = {
       title: "Creative Portfolio",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-3.jpeg",
+        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
     },
     {
       id: 2,
       title: "Minimal Single Product",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-2.jpeg",
+        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
     },
     {
       id: 3,
       title: "Strong Together Charity",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-4.jpeg",
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
     }
   ],
   illustrations: [
@@ -28,21 +28,21 @@ export const items = {
       title: "Creative Portfolio",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-1.jpeg",
+        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
     },
     {
       id: 2,
       title: "Minimal Single Product",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-2.jpeg",
+        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
     },
     {
       id: 3,
       title: "Strong Together Charity",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-3.jpeg",
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
     }
   ],
   Websites: [
@@ -51,21 +51,21 @@ export const items = {
       title: "Creative Portfolio",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-1.jpeg",
+        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
     },
     {
       id: 2,
       title: "Minimal Single Product",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-2.jpeg",
+        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
     },
     {
       id: 3,
       title: "Strong Together Charity",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "public/data/data-3.jpeg",
+        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
     }
   ],
 };
